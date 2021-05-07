9b0e0618b45e7f50fad9d859493bfa934edf2eda
Praveen Kuntumalla <pkuntumalla@us.ibm.com>
2021-05-07 15:03:58 -0400

    auto-increment id values, closes #21

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -21,13 +21,11 @@ public class DataLoader  implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Praveen");
         owner1.setLastName("Kuntumalla");
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Hruday");
         owner2.setLastName("Kuntumalla");
         ownerService.save(owner2);
@@ -35,13 +33,11 @@ public class DataLoader  implements CommandLineRunner {
         System.out.println("Loaded Owners...");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Kalpana");
         vet1.setLastName("Vallabhaneni");
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Anil");
         vet2.setLastName("Vallabhaneni");
         vetService.save(vet2);
